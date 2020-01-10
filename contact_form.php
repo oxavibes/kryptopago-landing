6c65d0e0a29df977f9d46788c31c68fb39a715d3
Stefano <katteos@hotmail.com>
2020-01-10 13:50:23 -0400

    Updating email credentials

--- contact_form.php
+++ contact_form.php
@@ -11,7 +11,7 @@ require 'vendors/phpmailer/src/SMTP.php';
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
-$to = "kryptopayapp@gmail.com";
+$to = "soporte@tubotones.com";
 $from = $_REQUEST['email'];
 $name = $_REQUEST['name'];
 $csubject = $_REQUEST['subject'];
@@ -21,10 +21,10 @@ try {
     //Server settings
     $mail->SMTPDebug = 2;                                       // Enable verbose debug output
     $mail->isSMTP();                                            // Set mailer to use SMTP
-    $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+    $mail->Host       = 'smtp.zoho.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'kryptopayapp@gmail.com';               // SMTP username
-    $mail->Password   = 'qy37JEBt6EP62UHc';                     // SMTP password
+    $mail->Username   = 'soporte@tubotones.com';               // SMTP username
+    $mail->Password   = 'soportetubotones';                     // SMTP password
     $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
